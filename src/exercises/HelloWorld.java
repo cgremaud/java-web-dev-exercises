b001f5027ccff539d5427bf222f832524809204a
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-27 15:02:11 -0500

    completed find area of rectangle exercise

--- src/exercises/HelloWorld.java
+++ src/exercises/HelloWorld.java
@@ -2,11 +2,11 @@ package exercises;
 import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String [] args){
+    public static void main(String[] args){
         Scanner input;
         input = new Scanner(System.in);
         System.out.println("What is your name?");
         String name = input.nextLine();
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + "!");
     }
 }
