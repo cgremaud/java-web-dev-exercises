dabe12d3b7e0c349e4bda6dd0bcab1f5035ef0d8
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-29 08:48:45 -0500

    finished hashmap student id practice

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -7,7 +7,7 @@ public class HashMapPractice {
     public static void main(String[] args) {
         HashMap<Integer, String> students = new HashMap<>();
         Scanner input = new Scanner(System.in);
-        Integer newStudentId;
+        int newStudentId; //initially had this as Integer, but I think autoboxing means that it can be the prim type.
         String newStudentName;
 
         System.out.println("Enter your students (or press ENTER to quit): ");
