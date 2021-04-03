7ab63c1a8e0247ad9f0fde98047aad1363355225
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-02 21:54:48 -0500

    testGasTankAfterExceeding. . . works

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -18,6 +18,7 @@ public class CarTest {
     public void createCarObject() {
         test_car = new Car("Toyota", "Prius", 10, 50);
     }
+
     @Test
     public void testInitialGasTank() {
         assertEquals(10, test_car.getGasTankLevel(), .001);
@@ -29,6 +30,11 @@ public class CarTest {
         assertEquals(9, test_car.getGasTankLevel(), .001);
     }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(501);
+        assertEquals(0, test_car.getGasTankLevel(), .0001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
 
 }
